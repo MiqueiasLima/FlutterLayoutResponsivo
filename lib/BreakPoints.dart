e7c4a4de2ddf490db7db6785a3944b97fd5996ed
MiqueiasLima <miqueiaslima@outlook.com>
2021-06-23 09:08:06 -0300

    Adicionando área para o conteúdo no body

---
+++ lib/BreakPoints.dart
@@ -0,0 +1,2 @@
+
+const breakPoints = 700;
--- lib/pages/home/HomePage.dart
+++ lib/pages/home/HomePage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:responsividade/pages/home/widgets/appbar/Mobile_app_bar.dart';
 import 'package:responsividade/pages/home/widgets/appbar/Web_App_Bar.dart';
+import '../../BreakPoints.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -10,9 +11,21 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints){
         return Scaffold(
-          appBar: constraints.maxWidth < 800 ? PreferredSize(child: Mobile_App_Bar(), preferredSize: Size(double.infinity,56)) :
+          appBar: constraints.maxWidth < breakPoints ? PreferredSize(child: Mobile_App_Bar(), preferredSize: Size(double.infinity,56)) :
           PreferredSize(child: Web_App_Bar(), preferredSize: Size(double.infinity,72)),
-          drawer: Drawer(),
+          drawer: constraints.maxWidth > breakPoints ? null : Drawer(),
+          body: Align(
+            alignment: Alignment.topCenter,
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                maxWidth: 1400,
+
+              ),
+              child: Container(
+                color: Colors.red,
+              ),
+            ),
+          ),
         );
     });
   }
