1a2a7bb31075636d79a2c4ff55f41b5c8255ce82
MiqueiasLima <miqueiaslima@outlook.com>
2021-06-23 15:09:34 -0300

    Adicionando card p/ Tablet

--- lib/pages/home/widgets/sections/Top_Sections.dart
+++ lib/pages/home/widgets/sections/Top_Sections.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:responsividade/BreakPoints.dart';
 import 'package:responsividade/pages/home/widgets/Custom_Search_Fill.dart';
 
 class Top_Section extends StatelessWidget {
@@ -44,13 +46,60 @@ class Top_Section extends StatelessWidget {
                           ],
                         ),
                       ),
-                    ))
+                    )
+                )
 
               ],
             )
 
           );
         }
+
+        if(resolucao >= breakPoints){
+          return SizedBox(
+            height: 320,
+            child: Stack(
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  height: 250,
+                  child: Image.asset("../../../imagens/logo.png",fit: BoxFit.cover,),
+                ),
+                Positioned(
+                    top: 25,
+                    left: 25,
+                    child: Card(
+                      color: Colors.black,
+                      elevation: 8,
+                      child: Container(
+                        padding: EdgeInsets.all(20),
+                        width: 400,
+                        child: Column(
+                          children: <Widget>[
+                            Text("Aprenda Flutter com este curso",style: TextStyle(
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),),
+                            Padding(padding: EdgeInsets.only(top: 10)),
+                            Text("Aprendendo Flutter com o profesor Daniel Ciolfi, "
+                                "cursos por apenas R\$ 22.90, qualidade garantida",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                              ),),
+                            Padding(padding: EdgeInsets.only(top: 10)),
+                            Custom_Search_Fill()
+                          ],
+                        ),
+                      ),
+                    )
+                ),
+              ],
+            ),
+          );
+        }
+
         return Container();
 
     });
